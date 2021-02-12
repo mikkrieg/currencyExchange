282d63c0c8a11ac34864c3d43b14f2c4b38be4af
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-12 09:26:06 -0800

    Change title in package.json, add amount conversion to API call

--- src/js/getCurrency.js
+++ src/js/getCurrency.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
-  static getCurrency(currency1, currency2){
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}`)
+  static getCurrency(currency1, currency2, amount){
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currency1}/${currency2}/${amount}`)
       .then(function(response){
         if(!response.ok){
           throw Error(response.status);
@@ -8,7 +8,7 @@ export default class CurrencyService {
         return response.json();
       })
       .catch(function(error){
-        return error
-      })  
+        return error;
+      });  
   } 
 }
