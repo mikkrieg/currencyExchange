d318da0611010571ed04be18eddcbcac65851efd
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-12 08:29:06 -0800

    Add notes to main.js for reference on objectives

--- src/main.js
+++ src/main.js
@@ -2,3 +2,35 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONALITY NESSECCARY
+// A user should be able to enter in US dollars and get a different currency in return using the API
+// Users should be able to convert US currency to atleast 5 others
+// Error handling for API call
+// API key in .env file
+// Message for if a user enters a currency that doesn't exist
+
+// MAIN OBJECTIVES
+// Correctly call API
+//  Correctly parse data
+//  error handling
+//  follow setup, store API key, add instructions to README
+// seperate logic into multipe files, use a static method
